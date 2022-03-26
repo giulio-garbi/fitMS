2dce1c22f62d0001a013d942ade7a3a98adf721d
Giulio <giulio.garbi@imtlucca.it>
2022-03-26 14:49:07 +0000

    JNI

---
+++ jni_lib/gccInstr.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gcc -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -fPIC -shared jni_GetThreadID.c -o libGetThreadID.so
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
+export LD_LIBRARY_PATH
+
